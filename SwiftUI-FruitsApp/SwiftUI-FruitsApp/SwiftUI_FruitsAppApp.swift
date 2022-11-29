a9480ea846755076e0422f472b08edac365fd439
nitin-agam <nitinagam17@gmail.com>
2022-11-29 20:59:57 +0530

    Project created

---
+++ SwiftUI-FruitsApp/SwiftUI-FruitsApp/SwiftUI_FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FruitsAppApp.swift
+//  SwiftUI-FruitsApp
+//
+//  Created by Nitin Aggarwal on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
